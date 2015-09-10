6b43f4f84c5f2ce51ea12caba839d86118f4b732
Bill Pond <bill.pond@isobar.com>
2015-09-10 15:05:57 -0400

    Dynamically add data to tennisTable

--- js/index.js
+++ js/index.js
@@ -1,8 +1,27 @@
 $(document).ready(function(){
     var friendshipDays = getFriendshipDays(); 
     $('footer p b').html(friendshipDays);
+
+    buildTable();
 }); 
 
+function buildTable(){
+    $.getJSON( "http://localhost:8000/data/notour.json", function( data ) {
+        var tbody = $("<tbody />"); 
+        for(var i = 0; i < data.length; i++) {
+            var row = $("<tr />");
+            row.html("<td>"+data[i].id+"</td>"+
+                "<td>"+data[i].player1+"</td>"+
+                "<td>"+data[i].player2+"</td>"+
+                "<td>"+data[i].winner+"</td>"+
+                "<td>"+data[i].score+"</td>"); 
+
+            row.appendTo(tbody);
+        }
+        var $tennisTable = $('#tennisTable');
+        tbody.appendTo($tennisTable);
+    });  
+}
 function getFriendshipDays(){
     var oneDay = 86400000; //one day in milliseconds bro
     var firstDate = new Date(2010,08,05);
