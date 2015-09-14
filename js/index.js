2a41c26639488191032df3f457a78aa3e88e4f55
Bill Pond <bill.pond@isobar.com>
2015-09-14 13:39:44 -0400

    Add beginnings of post call to node

--- js/index.js
+++ js/index.js
@@ -6,21 +6,42 @@ $(document).ready(function(){
 }); 
 
 function buildTable(){
+    var content = '[{"id":"1","player1":"Bill","player2":"Jordan","winner":"Bill","score": "20-2"}]';
+
+    $.ajax({
+        type: 'POST',
+        data: JSON.stringify(content),
+        contentType: 'application/json',
+        url: 'http://localhost:3000/additem',                      
+        success: function(data) {
+            console.log('success');
+            console.log(JSON.stringify(data));
+        }
+    });
+
     $.getJSON( "/data/notour.json", function( data ) {
         var tbody = $("<tbody />"); 
-        for(var i = 0; i < data.length; i++) {
-            var row = $("<tr />");
-            row.html("<td>"+data[i].id+"</td>"+
-                "<td>"+data[i].player1+"</td>"+
-                "<td>"+data[i].player2+"</td>"+
-                "<td>"+data[i].winner+"</td>"+
-                "<td>"+data[i].score+"</td>"); 
 
-            row.appendTo(tbody);
+        if(data != null && data.length > 0){
+            data = JSON.parse(data);
+            for(var i = 0; i < data.length; i++) {
+                var row = $("<tr />");
+                row.html("<td>"+data[i].id+"</td>"+
+                    "<td>"+data[i].player1+"</td>"+
+                    "<td>"+data[i].player2+"</td>"+
+                    "<td>"+data[i].winner+"</td>"+
+                    "<td>"+data[i].score+"</td>"); 
+
+                row.appendTo(tbody);
+            }
+            var $tennisTable = $('#tennisTable');
+            tbody.appendTo($tennisTable);
         }
-        var $tennisTable = $('#tennisTable');
-        tbody.appendTo($tennisTable);
-    });  
+        else{
+            $("<h1> There are currently no tennis games! </h1>").insertBefore("#tennisTable");
+            $("#tennisTable").remove(); 
+        }
+    }); 
 }
 function getFriendshipDays(){
     var oneDay = 86400000; //one day in milliseconds bro
--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 var express = require('express');
 var app = express();
 var path = require("path");
+var fs = require('fs');
+var bodyParser = require('body-parser');
+
 
 var dirname = path.join(__dirname, './');
 console.log(dirname);
 app.use(express.static(dirname));
+app.use(bodyParser.urlencoded({ extended: false })) 
 
-app.get('/tennis', function (req, res) {
-  res.sendFile(path.join(dirname+'/index.html'));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(dirname+'/index.html'));
+});
+
+app.post('/additem', function (req, res, next) {
+    var fileName = "./data/notour.json";
+    console.log('body: ' + JSON.stringify(req.body));
+    fs.writeFileSync(fileName, JSON.stringify(req.body));
+    res.send(fileName);
 });
 
 var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Example app listening at http://%s:%s', host, port);
 }); 
