4ffa03f86a7cf4319f24cf62dbe2733b75151d94
Bill Pond <bill.pond@isobar.com>
2015-09-14 10:26:33 -0400

    Added grunt workflow and add game button/modal

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,6 +15,13 @@ module.exports = function(grunt) {
             }
         },
         watch: {
+            express: {
+                files:  [ '**/*.js' ],
+                tasks:  [ 'express:dev' ],
+                options: {
+                spawn: false // for grunt-contrib-watch v0.5.0+, "nospawn: true" for lower versions. Without this option specified express won't be reloaded
+                }
+            }, 
             styles: {
                 files: ['less/**/*.less'], // which files to watch
                 tasks: ['less'],
@@ -22,8 +29,29 @@ module.exports = function(grunt) {
                     nospawn: true
                 }
             }
+        },
+        express: {
+            options: {
+              // Override defaults here
+            },
+            dev: {
+              options: {
+                script: 'server.js'
+              }
+            },
+            prod: {
+              options: {
+                script: 'server.js',
+                node_env: 'production'
+              }
+            },
+            test: {
+              options: {
+                script: 'server.js'
+              }
+            }
         }
     });
-
-    grunt.registerTask('default', ['less', 'watch']);
+    grunt.loadNpmTasks('grunt-express-server');
+    grunt.registerTask('default', [ 'express:dev', 'watch', 'less']);
 };
